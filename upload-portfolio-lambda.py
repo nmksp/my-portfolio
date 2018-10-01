acd585eaf8a839778029c0d4cc96d40b42857ca9
nmksp <surriyaprasath@gmail.com>
2018-10-01 20:12:50 +1000

    Updated portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,4 +1,3 @@
-
 # importing some modules.
 import boto3
 import StringIO
@@ -6,22 +5,25 @@ import zipfile
 import mimetypes
 from botocore.client import Config
 
+def lambda_handler(event, context):
 
-#creating name for s3 resource
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    #creating name for s3 resource
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-#creating names for both of the buckets.
-portfolio_bucket = s3.Bucket('portfolio.surriya')
-build_bucket = s3.Bucket('portfolio-build.surriya')
+    #creating names for both of the buckets.
+    portfolio_bucket = s3.Bucket('portfolio.surriya')
+    build_bucket = s3.Bucket('portfolio-build.surriya')
 
-# Creating portfolio for memory
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    # Creating portfolio for memory
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-# Using the zip file to extract it, upload and set the ACL.
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    # Using the zip file to extract it, upload and set the ACL.
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return "Hello from lambda."
