96925cc6450bba721f78b9f02769a539f56b8f54
nmksp <surriyaprasath@gmail.com>
2018-10-03 07:10:27 +1000

    Updated code in upload-portfolio-lambda.py file

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,6 +9,66 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:963211384567:deployPortfolioTopic')
 
+    location = {
+        "bucketName":'portfolio-build.surriya',
+        "objectKey": 'portfoliobuild.zip',
+    }
+    try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+
+        #creating name for s3 resource
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+        #creating names for both of the buckets.
+        portfolio_bucket = s3.Bucket('portfolio.surriya')
+        build_bucket = s3.Bucket(location["bucketName"])
+
+        # Creating portfolio for memory
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+
+        # Using the zip file to extract it, upload and set the ACL.
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done! deployed using CodePipeline"
+        topic.publish(Subject = "Portfolio Deployed", Message = "Portfolio has been deployed successfully by CodePipeline. Check out your new website.")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+    except:
+        topic.publish(Subject = "Portfolio Deploy Failed", Message = "Portfolio was not deployed by CodePipeline. Check what went wrong.")
+        raise
+    return "Hello from lambda."
+
+
+
+ ###########
+# OLD CODE  #
+ ###########
+
+# importing some modules.
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+from botocore.client import Config
+
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:963211384567:deployPortfolioTopic')
+
     try:
         #creating name for s3 resource
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
